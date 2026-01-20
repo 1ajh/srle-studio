0c14bf41dd0574a6c726cbfa000a5e1bd20cb790
AJ H <105573909+1ajh@users.noreply.github.com>
2026-01-20 13:53:35 +0000

    fix: Try ffmpeg_kit_flutter base package

--- lib/services/ffmpeg_service.dart
+++ lib/services/ffmpeg_service.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
-import 'package:ffmpeg_kit_flutter_min/ffmpeg_kit.dart';
-import 'package:ffmpeg_kit_flutter_min/return_code.dart';
-import 'package:ffmpeg_kit_flutter_min/statistics.dart';
+import 'package:ffmpeg_kit_flutter/ffmpeg_kit.dart';
+import 'package:ffmpeg_kit_flutter/return_code.dart';
+import 'package:ffmpeg_kit_flutter/statistics.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' as path;
 import '../models/effect_mode.dart';
